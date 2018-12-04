ab59aed200db218e786372727fa68ee9d65c7c17
Roberto Carrasco <roberto0_9@hotmail.com>
2018-12-03 22:13:16 -0300

    add new function

--- main.cu
+++ main.cu
@@ -12,7 +12,19 @@ const int WMMA_N = 16;
 const int WMMA_K = 16;
 
 __global__ void matmuls_tc(half* A, half* B, float *C, int n){
+    int off = blockIdx.x*TCSIZE*TCSIZE;
 
+    wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half,wmma::row_major> a_frag;
+    wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half,wmma::row_major> b_frag;
+    wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> c_frag;
+   
+    wmma::load_matrix_sync(a_frag, A + off, TCSIZE);
+    wmma::load_matrix_sync(b_frag, B + off, TCSIZE);
+    wmma::fill_fragment(c_frag, 0.0f);
+
+    wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
+
+    wmma::store_matrix_sync(C + off, c_frag, TCSIZE, wmma::mem_row_major);
 }
 
 __global__ void convertFp32ToFp16 (half *out, float *in, int n) {
@@ -25,9 +37,11 @@ __global__ void convertFp32ToFp16 (half *out, float *in, int n) {
  __global__ void matmuls_basic(float* A, float* B, float *C, int n){
     int off = blockIdx.x * (TCSIZE*TCSIZE);
     int tid = off + (threadIdx.y*TCSIZE + threadIdx.x);
+    float sum = 0.0f;
     for(int i=0; i<TCSIZE; ++i){
-        C[tid] += A[off + threadIdx.y*TCSIZE + i]*B[off + i*TCSIZE + threadIdx.x];
+        sum += A[off + threadIdx.y*TCSIZE + i]*B[off + i*TCSIZE + threadIdx.x];
     }
+    C[tid] = sum;
 }
 
 __global__ void matmuls_basic_half(half* A, half* B, float* C, int n){
@@ -111,7 +125,10 @@ int main(int argc, char **argv){
         block = dim3(TCSIZE, TCSIZE, 1);
         grid = dim3((totaln+TCSIZE*TCSIZE-1)/(TCSIZE*TCSIZE), 1, 1);
         cudaEventRecord(start);
-        matmuls_basic<<<grid, block>>>(Ad, Bd, Cd, totaln);
+        for(int i=0;i<100;++i){
+            matmuls_basic<<<grid, block>>>(Ad, Bd, Cd, totaln);
+            cudaDeviceSynchronize();   
+        }
         cudaEventRecord(stop);
         cudaEventSynchronize(stop);
     }
@@ -119,30 +136,36 @@ int main(int argc, char **argv){
         block = dim3(TCSIZE, TCSIZE, 1);
         grid = dim3((totaln+TCSIZE*TCSIZE-1)/(TCSIZE*TCSIZE), 1, 1);
         cudaEventRecord(start);
-        matmuls_basic_half<<<grid, block>>>(Adh, Bdh, Cd, totaln);
+        for(int i = 0; i<100; ++i){
+            matmuls_basic_half<<<grid, block>>>(Adh, Bdh, Cd, totaln);
+            cudaDeviceSynchronize();
+        }
         cudaEventRecord(stop);
     }
     if(alg == 2){    
-        block = dim3(TCSIZE, 2, 1);
+        block = dim3(TCSIZE*2,1, 1);
         grid = dim3(nmats, 1, 1);
         cudaEventRecord(start);
-        matmuls_tc<<<grid, block>>>(Adh, Bdh, Cd, totaln);
+        for(int i=0;i<100; ++i){
+            matmuls_tc<<<grid, block>>>(Adh, Bdh, Cd, totaln);
+        }
         cudaEventRecord(stop);
     }
 
     cudaEventSynchronize(stop);
     float time = 0;
     cudaEventElapsedTime(&time, start, stop);
-    printf("%s: %f secs\n","matmuls" , time/1000.0f);
+    //printf("%s: %f secs\n","matmuls" , time/100000.0f);
+    printf("%i, %f, %i \n",alg , time/100000.0f, nmats);
 
     cudaMemcpy(A, Ad, sizeof(float)*totaln, cudaMemcpyDeviceToHost);
     cudaMemcpy(B, Bd, sizeof(float)*totaln, cudaMemcpyDeviceToHost);
     cudaMemcpy(C, Cd, sizeof(float)*totaln, cudaMemcpyDeviceToHost);
 
-    if(nmats < PRINTLIMIT){
+/*    if(nmats < PRINTLIMIT){
         printmats(C, nmats, "[after] mat C:");
     }
-
+*/
     free(A);
     free(B);
     free(C);
